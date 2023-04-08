ad7d412d95f269ae527e8fd87985f55748b8e511
naime <naimeh461@gmail.com>
2023-04-09 05:03:10 +0600

    first commit

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header/Header';
+import { useLoaderData } from 'react-router-dom';
+
+const Home = () => {
+    const tShirts = useLoaderData();
+    console.log(tShirts);
+    return (
+        <div>
+            <h2>this is home</h2>
+        </div>
+    );
+};
+
+export default Home;
